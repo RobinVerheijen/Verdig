93a8068a59f9e708f286f0af0b4834f0c604437f
Robin Verheijen <robin.verheijen@hotmail.com>
2014-02-10 15:10:26 +0100

    Listen to hash changes

--- js/index.js
+++ js/index.js
@@ -3,7 +3,21 @@ $(document).ready(function() {
         anchors: ['what-we-do', 'who-we-are', 'portfolio', 'contact'],
         navigation: true,
         navigationPosition: 'right',
+        slidesNavigation: false
 //        navigationTooltips: ['Wat doen wij', 'Wie zijn wij', 'Portfolio', 'Contact']
+    });
+    
+    if(window.location.hash) {
+        $(window.location.hash + '-listitem').addClass('active');
+    }
+    
+    $(window).on('hashchange', function() {        
+        $('li.active').removeClass('active');
+        $(window.location.hash + '-listitem').addClass('active');
+    });
+    
+    $('#imac-image').click(function() {
+        $.fn.fullpage.moveTo('what-we-do', 1);
     });
 });
 
