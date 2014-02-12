75a460e9d8f43d3a950b10a67e4a2cab457edf50
Saron Grave <iphone.grave@gmail.com>
2014-02-12 13:12:30 +0100

    lots of changes, menu and img

--- js/index.js
+++ js/index.js
@@ -11,34 +11,24 @@ $(document).ready(function() {
                 $('#saron, #robin').addClass('active');
             }
 //            $('#infoMenu').toggleClass('whiteLinks', index == 4);
-
         }
 //        navigationTooltips: ['Wat doen wij', 'Wie zijn wij', 'Portfolio', 'Contact']
     });
 
-    $('#logo-image').addClass('bounceIn animated');
+    $('#logo').addClass('bounceIn animated');
 
     if(window.location.hash) {
         $(window.location.hash + '-listitem').addClass('active');
+    } else {
+        $("#home-listitem").addClass("active");
     }
-    
-    $(window).on('hashchange', function() {        
+
+    $(window).on('hashchange', function() {
         $('li.active').removeClass('active');
         $(window.location.hash + '-listitem').addClass('active');
     });
-    
+
     $('#imac-image').click(function() {
         $.fn.fullpage.moveTo('what-we-do', 1);
     });
-});
-
-// menubar animation
-$(document).scroll(function() {
-  if ($(document).scrollTop() >= 80) {
-      $(".ca-menu").addClass("full");
-      $("#menu-container").css("position", "fixed");
-  } else {
-      $("#menu-container").css("position", "absolute");
-      $(".ca-menu").removeClass("full");
-  }
 });
