f47d9c239b94ade302f9be75c0573f2b6e98d400
Saron Grave <iphone.grave@gmail.com>
2014-04-01 13:41:41 +0200

    splitlayout done

--- js/SplitLayout.js
+++ js/SplitLayout.js
@@ -10,55 +10,37 @@
  * http://www.codrops.com
  */
 $(document).ready(function() {
-    var splitlayout = $('#splitlayout'),
-            leftSide = $('.side-left'),
-            rightSide = $('.side-right'),
-            pageLeft = $('.page-left'),
-            pageRight = $('.page-right'),
-            transEndEventNames = {
-                'WebkitTransition': 'webkitTransitionEnd',
-                'MozTransition': 'transitionend',
-                'OTransition': 'oTransitionEnd',
-                'msTransition': 'MSTransitionEnd',
-                'transition': 'transitionend'
-            },
-    transEndEventName = transEndEventNames[Modernizr.prefixed('transition')];
+    $('.back-left').on('click', function(ev) {close(ev);});
+    $('.back-right').on('click', function(ev) {close(ev);});
 
-    if (mobilecheck()) {
-        $('.splitlayout').addClass('mobile-layout');
-    }
-    $('.splitlayout').addClass('reset-layout');
-
-    $('.side-left').on('click', function() {
-        reset();
-        $('.splitlayout').addClass('open-left');
+    $('.side-left').on('click', function(ev) {
+        if ($('.splitlayout').hasClass('open-left')) {
+            close(ev);
+        } else {
+            reset();
+            $('.splitlayout').addClass('open-left');
+        }
     });
 
-    $('.side-right').on(click, function() {
-        reset();
-        $('.splitlayout').addClass('open-right');
+    $('.side-right').on('click', function(ev) {
+        if ($('.splitlayout').hasClass('open-right')) {
+            close(ev);
+        } else {
+            reset();
+            $('.splitlayout').addClass('open-right');
+        }
     });
-
-    // back to intro after transition ends:
-    var onEndTransFn = function() {
-        this.removeEventListener(transEndEventName, onEndTransFn);
-        $('.splitlayout').addClass('reset-layout');
-    },
-            backToIntro = function(ev) {
-                ev.preventDefault();
-                ev.stopPropagation();
-                var dir = $(ev.target).hasClass('back-right') ? 'left' : 'right', page = dir === 'right' ? pageRight : pageLeft;
-                $('.splitlayout').removeClass('open-' + dir);
-                $('.splitlayout').addClass('close-' + dir);
-                $('.' + dir).on(transEndEventName, onEndTransFn);
-            };
-
-    $('.back-left').on(eventtype, backToIntro);
-    $('.back-right').on(eventtype, backToIntro);
 });
 
 function reset() {
     $('.splitlayout').removeClass('close-right');
     $('.splitlayout').removeClass('close-left');
-    $('.splitlayout').removeClass('close-layout');
+}
+
+function close(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+    dir = $('.splitlayout').hasClass('open-right') ? 'right' : 'left';
+    $('.splitlayout').removeClass('open-' + dir);
+    $('.splitlayout').addClass('close-' + dir);
 }
